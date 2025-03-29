0403f73fc4a12fd09d479710a986ccd3f8037100
Akshat Garg <akshat.23bce10641@vitbhopal.ac.in>
2025-03-29 15:25:26 +0530

    Solved Issues

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,20 @@ import { Row, Col } from 'react-bootstrap';
 
 function Header() {
   return (
-    <Row className="mb-5">
+    <Row className="mb-5 text-center">
       <Col>
-        <h1 className="display-3 pacifico-font birthday-heading fadeIn delay-1">
-          Happy Birthday!
-        </h1>
-        <h2 className="display-4 pacifico-font text-primary mb-4 fadeIn delay-2">
+        <div className="mb-3">
+          <h1 style={{
+            fontFamily: "'Pacifico', cursive",
+            fontSize: "5rem",
+            color: "#ff6b6b",
+            marginBottom: "1rem",
+            textShadow: "3px 3px 0px rgba(0,0,0,0.1)"
+          }}>
+            Happy Birthday!
+          </h1>
+        </div>
+        <h2 className="display-4 pacifico-font text-primary mb-4">
           Harshita Sharma
         </h2>
       </Col>
